19945b5acd2ca26f95ff087cdbe368111e8ec055
Hajime Hoshi <hajimehoshi@gmail.com>
2020-04-12 17:33:32 +0900

    tour/hello_world: Update for 1.11

--- docs/go/helloworld/main.go
+++ docs/go/helloworld/main.go
@@ -7,16 +7,24 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
-func update(screen *ebiten.Image) error {
-	if ebiten.IsDrawingSkipped() {
-		return nil
-	}
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+type Game struct{}
+
+func (g *Game) Update(screen *ebiten.Image) error {
 	return nil
 }
 
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
 func main() {
-	if err := ebiten.Run(update, 320, 240, 2, "Hello, World!"); err != nil {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
 }
